28e0e062b09c83d1f4811d20c45ca2fb3732f3d2
Eduardo Tonatto <edtonatto@gmail.com>
2022-07-06 23:22:11 -0300

    :sparkles: Realizado importações das rotas para novos endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { componentsRoutes } from "./components.routes";
+import { modelsRoutes } from "./models.routes";
 
 const router = Router();
 
 router.use('/components', componentsRoutes);
+router.use('/models', modelsRoutes);
 
 export { router };
