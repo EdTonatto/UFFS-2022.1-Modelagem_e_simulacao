3ce1508c4c5c1f907314d1c7c20c82171b4b297a
Eduardo Tonatto <edtonatto@gmail.com>
2022-07-06 23:22:43 -0300

    :zap: Adicionado propriedade de ID para os Component

--- src/modules/component/entities/Component.ts
+++ src/modules/component/entities/Component.ts
@@ -1,13 +1,21 @@
 import { Interval } from "./Interval"
 import { Next } from "./Next"
+import { v4 as uuidV4 } from 'uuid';
 
 class Component {
+    id: string;
     name: string;
     qtyServer: number;
     randomTimeServer: Interval[];
     flagBusy: Boolean[];
     timeInOut: Interval[];
     next: Next[];
+
+    constructor() {
+        if (!this.id) {
+          this.id = uuidV4();
+        }
+    }
 }
 
 export { Component }
