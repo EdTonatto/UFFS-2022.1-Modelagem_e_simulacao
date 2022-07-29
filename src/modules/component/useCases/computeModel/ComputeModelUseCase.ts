d7a26754f5784cca9b3541043f214f15d0e47e28
Eduardo Tonatto <edtonatto@gmail.com>
2022-07-29 17:09:11 -0300

    :construction: Iniciado implementação de calculo dos componentes

--- src/modules/component/useCases/computeModel/ComputeModelUseCase.ts
+++ src/modules/component/useCases/computeModel/ComputeModelUseCase.ts
@@ -1,13 +1,31 @@
+import { getNextComponent } from "../../../../utils/nextComponent";
 import { randomNumberWithInterval } from "../../../../utils/numbers";
+import { Component } from "../../entities/Component";
+import { Interval } from "../../entities/Interval";
+import { TemporaryUser } from "../../entities/TemporaryUser";
 import { IModelsRepository } from "../../repositories/IModelsRepository";
+import { ComponentsRepository } from "../../repositories/implementations/ComponentsRepository";
 
 class ComputeModelUseCase{
     constructor(private modelsRepository: IModelsRepository){}
 
+    computeComponent(temporaryUser: TemporaryUser, component: Component){
+
+    }
+
     execute(id: string){
-              
+        const model = this.modelsRepository.findById(id);
+        const initialComponent = model.components[0];
+        const temporaryUsers: TemporaryUser[] = [];
+        let time = 0;
+
         for (let i = 0; i < model.qtyUser; i++) {
-            console.log(randomNumberWithInterval(model.components[0].randomTimeServer[0].initial, model.components[0].randomTimeServer[0].final));
+            const temporaryUser = new TemporaryUser();
+            time = time + randomNumberWithInterval(initialComponent.randomTimeServer[0].initial, 
+                                                   initialComponent.randomTimeServer[0].final);
+            temporaryUser.name = `User ${i}`;
+            temporaryUser.timeInOut.initial = time;
+            this.computeComponent(temporaryUser, ComponentsRepository.getInstance().findByName(getNextComponent(initialComponent)));
         }
     }
 }
