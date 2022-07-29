8cd353c8d57606144ef3513001113d0bec10d813
Eduardo Tonatto <edtonatto@gmail.com>
2022-07-29 18:05:13 -0300

    :zap: Adicionado validação para somente buscar nome do próximo caso exista

--- src/utils/nextComponent.ts
+++ src/utils/nextComponent.ts
@@ -4,11 +4,13 @@ import { between, randomNumberWithInterval } from "./numbers";
 function getNextComponent(components: Component): string {
     const randomNumber = randomNumberWithInterval(0, 100);
     let nextName = '';
-    components.next.forEach(nextComponent => {
-        if(between(randomNumber, nextComponent.percent.initial, nextComponent.percent.final)){
-            nextName = nextComponent.name;
-        }
-    });
+    if(components.next){
+        components.next.forEach(nextComponent => {
+            if(between(randomNumber, nextComponent.percent.initial, nextComponent.percent.final)){
+                nextName = nextComponent.name;
+            }
+        });
+    }
     return nextName;
 }
 
