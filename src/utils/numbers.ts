2dc66b480f5f64f5144f4449ccd3c88c5e051920
Eduardo Tonatto <edtonatto@gmail.com>
2022-07-29 17:06:59 -0300

    :sparkles: Adicionado método que valida numero entre outros

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -1,5 +1,9 @@
+function between(x: number, min: number, max: number) {
+    return x >= min && x <= max;
+}
+
 function randomNumberWithInterval(min: number, max: number): number{
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-export { randomNumberWithInterval }
+export { between, randomNumberWithInterval }
